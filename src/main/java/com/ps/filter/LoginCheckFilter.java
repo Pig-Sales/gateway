5d527b057aed7310cdaf326d8417c0e50a1d7f88
Cbgogogog <1293543528@qq.com>
2023-07-24 18:36:08 +0800

    feat: 增加gateway全局过滤器

---
+++ src/main/java/com/ps/filter/LoginCheckFilter.java
@@ -0,0 +1,64 @@
+package com.ps.filter;
+
+import com.ps.utils.JwtUtils;
+import io.jsonwebtoken.Claims;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.core.Ordered;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+@Slf4j
+@Component
+public class LoginCheckFilter implements GlobalFilter, Ordered {
+
+    @Override
+    public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain filterChain) {
+        ServerHttpRequest req=  exchange.getRequest();
+        ServerHttpResponse resp = exchange.getResponse();
+
+        //1.获取url
+        String url = req.getURI().getPath();
+        log.info("请求的url:{}",url);
+
+        //2.判断url是否是login或register
+        if(req.getMethod().toString().equals("POST")){
+            if(url.equals("/user/login")){
+                log.info("登录操作直接放行");
+                return filterChain.filter(exchange);
+            }
+        }
+
+        //3.获取请求头令牌(token)
+        String jwt = req.getHeaders().getFirst("Authorization");
+
+        //4.判断令牌是否存在，不存在返回错误结果，未登录
+        if(!StringUtils.hasLength(jwt)){
+            log.info("请求头token为空，返回未登录信息");
+            resp.setStatusCode(HttpStatus.UNAUTHORIZED);
+            return exchange.getResponse().setComplete();
+        }
+//       5.解析令牌，如果失败，返回错误结果
+        try {
+            Claims claims = JwtUtils.parseJWT(jwt);
+        } catch (Exception e) {//解析失败
+            log.info("解析令牌失败，返回未登录错误信息");
+            resp.setStatusCode(HttpStatus.UNAUTHORIZED);
+            return exchange.getResponse().setComplete();
+        }
+//        6.放行
+        log.info("持有合法令牌，放行");
+        return filterChain.filter(exchange);
+    }
+
+    @Override
+    public int getOrder() {
+        return -1;
+    }
+}
